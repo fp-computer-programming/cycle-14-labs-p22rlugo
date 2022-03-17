188781efc36e1e63cbc87ddf6d73b20d70549f4b
p22rlugo <p22rlugo@fairfieldprep.org>
2022-03-17 09:52:38 -0400

    Add files

---
+++ lab_14-1.py
@@ -0,0 +1,19 @@
+# Ryan Lugo: 3/17/22
+
+import turtle
+
+window = turtle.Screen()
+window.setup(1000,1000)
+window.title("Lab 1")
+mark = turtle.Turtle()
+
+mark.right(90)
+mark.forward(250)
+mark.left(90)
+mark.forward(100)
+mark.left(90)
+mark.forward(250)
+mark.left(90)
+mark.forward(100)
+
+window.mainloop()
---
+++ lab_14-2.py
@@ -0,0 +1,19 @@
+# # Ryan Lugo: 3/17/22
+
+import turtle
+
+window = turtle.Screen()
+window.setup(500,500)
+window.title("Lab 2")
+mark = turtle.Turtle()
+
+mark.right(90)
+mark.forward(100)
+mark.left(90)
+mark.forward(100)
+mark.left(90)
+mark.forward(100)
+mark.left(90)
+mark.forward(100)
+
+window.mainloop()
